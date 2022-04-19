91d6d974d7c2004d6611de56fd3582a531b9ed40
rajibkn345 <rajibkn345@gmail.com>
2022-04-19 08:43:19 +0600

    adding some file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+//registerServiceWorker();
