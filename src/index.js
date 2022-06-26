096c5a1dbb740596eb482272f8a1ab8baff2d51e
Rajib Khan <rajibkn345@gmail.com>
2022-06-26 16:21:29 +0600

    some more modification

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 //import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import "tachyons";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 //registerServiceWorker();
